0ed1930fa2f357f87ce208c951cccc20ce98dfc2
Christopher <firedoglo1941@gmail.com>
2020-10-22 14:43:47 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  User_Settings: {
+    Sends_Random_Sentences: false, // If set to true, will send messages at the set User_Interval.
+    React_To_Mentions: false // If set to true, will react to @everyone, @here, and @user mentions.
+  },
+  User_Sentences: [], // The Sentences Sent Upon Mentioning at Random Selection.
+  User_Interval: 10, // The Set Time to Send Random Sentences. Default = 10.
+  User_Token: " " // Your Token Authorization Here. To automate your user account.
+}
