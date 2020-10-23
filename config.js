640902624afa2ed53b02ad95bec03d7db3cb6828
Christopher <firedoglo1941@gmail.com>
2020-10-23 04:41:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  Server_ID: "", // This is required, this is to have a targetted server so the bot doesn't go haywire with other servers.
   User_Settings: {
     Sends_Random_Sentences: false, // If set to true, will send messages at the set User_Interval.
     React_To_Mentions: false // If set to true, will react to @everyone, @here, and @user mentions.
