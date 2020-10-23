51ea0f92fedc29ae7f0c23afd0a47d67895cb855
Christopher <firedoglo1941@gmail.com>
2020-10-23 10:14:37 -0400

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -1,1 +1,8 @@
+module.exports = (Bot, Message) => { 
+    if (Message.author.bot) return;
+    if (Message.author.id !== Bot.user.id) return;
 
+    if (Message.guild.id == Settings.Config.Server_ID) {
+        // More Code Coming Soon.
+    }
+}
