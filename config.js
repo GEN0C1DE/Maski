c405842256edf660e4a5ec9acf99b556e6752c58
Christopher <firedoglo1941@gmail.com>
2020-11-16 05:22:55 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  Server_ID: "", // This is required, this is to have a targetted server so the bot doesn't go haywire with other servers.
+  Server_ID: "", // This is required, this is to have a targeted server so the bot doesn't go haywire with other servers.
   User_Settings: {
-    Sends_Random_Sentences: false, // If set to true, will send messages at the set User_Interval.
-    React_To_Mentions: false // If set to true, will react to @everyone, @here, and @user mentions.
+    Sends_Random_Sentences: false // If set to true, will send messages at the set User_Interval.
   },
+  User_Stauts: "", // If not enabled, or not set, then will defaulted to regular Maski Status.
   User_Sentences: [], // The Sentences Sent Upon Mentioning at Random Selection.
   User_Interval: 10, // The Set Time to Send Random Sentences. Default = 10.
-  User_Token: " " // Your Token Authorization Here. To automate your user account.
+  User_Token: "" // Your Token Authorization Here. To automate your user account.
 }
--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 @echo off
-npm install ---
+npm install fs discord.js-selfbot chalk txtgen
