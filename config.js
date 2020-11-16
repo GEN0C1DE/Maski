4637168a10fdddb3ff2e2b81f3ea3e538256291c
Christopher <firedoglo1941@gmail.com>
2020-11-16 12:00:44 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ module.exports = {
     Sends_Random_Sentences: false // If set to true, will send messages at the set User_Interval.
   },
   User_Stauts: "", // If not enabled, or not set, then will defaulted to regular Maski Status.
-  User_Sentences: [], // The Sentences Sent Upon Mentioning at Random Selection.
   User_Interval: 10, // The Set Time to Send Random Sentences. Default = 10.
   User_Token: "" // Your Token Authorization Here. To automate your user account.
 }
