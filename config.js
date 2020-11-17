20283090c3df634cbc27337b84d289b3ff341adc
Christopher <firedoglo1941@gmail.com>
2020-11-16 23:48:05 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  Server_ID: "777803436279332875", // This is required, this is to have a targeted server so the bot doesn't go haywire with other servers.
-  User_Settings: {
-    Sends_Random_Sentences: true // If set to true, will send messages at the set User_Interval.
-  },
-  User_Status: "Genocide on the Beat.", // If not enabled, or not set, then will defaulted to regular Maski Status.
-  User_Interval: 10, // The Set Time to Send Random Sentences. Default = 10.
-  User_Token: "Nzc4MTE1NDQ3NDU0MTcxMTM3.X7NSqQ.XAhS3R-8qWJMHLlfMdIbhw5eqh0" // Your Token Authorization Here. To automate your user account.
-}
