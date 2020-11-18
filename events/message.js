9357b4bb90fca862fb0a4755104d5d28274d7584
Christopher <firedoglo1941@gmail.com>
2020-11-18 00:58:43 -0500

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -11,13 +11,25 @@ module.exports = (Bot, Message) => {
     ]
 	
 	function SendingMessage() {
-		Message.channel.send("Not AFK")
-		.catch(ERR => {
-			Message.react("\uD83D\uDE00")
-			.catch(ERR2 => {
-				console.log(Chalk.red("MASKI: COULDN'T REACT OR SEND MESSAGE."))
+		let Sentences = Settings.Config.User_Settings.Sentences_Besides_Default
+		if (Sentences.length !== 0) {
+			let ChosenSentence = Sentences[Math.floor(Math.random() * Sentences.length)]
+			Message.channel.send(ChosenSentence)
+			.catch(ERR => {
+				Message.react("\uD83D\uDE00")
+				.catch(ERR2 => {
+					console.log(Chalk.red("MASKI: COULDN'T REACT OR SEND MESSAGE."))
+				})
 			})
-		})
+		} else {
+			Message.channel.send("Not AFK")
+			.catch(ERR => {
+				Message.react("\uD83D\uDE00")
+				.catch(ERR2 => {
+					console.log(Chalk.red("MASKI: COULDN'T REACT OR SEND MESSAGE."))
+				})
+			})
+		}
 	}
 	
     if (Message.guild.id == Settings.Config.Server_ID) {
